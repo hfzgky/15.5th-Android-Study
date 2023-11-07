63e5f0303352b097cd8ac88b9026710e31318e8c
choiMY <thskan123439@gmail.com>
2023-11-08 01:50:50 +0900

    docs: api 인증키 숨김

--- choi/app/src/main/java/com/example/study_choi/retrofit/TourSpotDJ.kt
+++ choi/app/src/main/java/com/example/study_choi/retrofit/TourSpotDJ.kt
@@ -1,12 +1,13 @@
 package com.example.study_choi.retrofit
 
+import com.example.study_choi.BuildConfig
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TourSpotDJ {
     companion object {
-        private const val AUTH_KEY = "LGRG0cSCo30ReUvNQDWhJbelChuEODhVAWcy0a7IeMWmsGhXzLTzC1qTlxlM5cm78fIu4Y8BYwl6wOF4TURejA=="
+        private const val AUTH_KEY = BuildConfig.TOUR_API_KEY
     }
 
     @GET("gettourspot")
