71c98bc7bd994eee39a2abe7e9b2db9decad7e85
choiMY <thskan123439@gmail.com>
2023-11-19 20:41:36 +0900

    design: todo 생성, todo 세부사항 ui 추가

---
+++ choi/app/src/main/java/com/example/study_choi/UserModel.kt
@@ -0,0 +1,3 @@
+package com.example.study_choi
+
+data class UserModel()
--- choi/app/src/main/java/com/example/study_choi/retrofit/TourDTO.kt
+++
@@ -1,9 +0,0 @@
-package com.example.study_choi.retrofit
-
-
-import com.google.gson.annotations.SerializedName
-
-data class TourDTO(
-    @SerializedName("response")
-    val response: Response
-)
